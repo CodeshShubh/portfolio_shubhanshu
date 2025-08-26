130bb6da5b8f22c707a2da60c4bd5f01bccd7276
Akashh Das <codeshshubh@gmail.com>
2025-08-26 06:50:55 +0530

    generateIndexSitemap: false

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://webdevshubhanshu.netlify.app', // replace with your site URL
   generateRobotsTxt: true, // (optional) generate robots.txt
+  generateIndexSitemap: false,  // 👈 this forces a single sitemap.xml but if we have mutiple page so we can remove this line
   sitemapSize: 7000,
   changefreq: 'daily',
   priority: 0.7,
